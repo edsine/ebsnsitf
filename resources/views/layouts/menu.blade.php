c7140c854eb2a6fa2f5f1cbc53eeac30b123c5ad
Omeiza Alabi <omeizaalabi@gmail.com>
2023-06-02 13:18:21 +0100

    menu change

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,7 +6,7 @@
     </a>
 </li>
 
-<li>
+<li class="nav-item">
     <a href="{{ route('users.index') }}" class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
         <i class="nav-icon fa fa-user"></i>
         <p>Users</p>
