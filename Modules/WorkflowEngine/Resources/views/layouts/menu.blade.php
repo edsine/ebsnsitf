b9db258bab70187b11d3a14d2fed95919b5d13e5
Abdul5050 <tacticshustle@gmail.com>
2023-06-14 15:33:29 +0100

    new updates

--- Modules/WorkflowEngine/Resources/views/layouts/menu.blade.php
+++ Modules/WorkflowEngine/Resources/views/layouts/menu.blade.php
@@ -85,13 +85,6 @@
 </li>
 
 <li class="nav-item">
-    <a href="{{ route('branches.index') }}" class="nav-link {{ Request::is('branches*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Branches</p>
-    </a>
-</li>
-
-<li class="nav-item">
     <a href="{{ route('departments.index') }}" class="nav-link {{ Request::is('departments*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Departments</p>
